4baed8e33c4183c5b4693f7937ff0b341a130ed1
Joseph Darville <joseph.darville9@gmail.com>
2022-06-22 22:12:25 -0400

    replaced all files to fix the inquirer problem

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -1,71 +1,59 @@
-const Enemy = require('../lib/Enemy');
-const Potion = require('../lib/Potion')
+const Enemy = require('../lib/Enemy.js');
+const Potion = require('../lib/Potion.js');
 
 jest.mock('../lib/Potion.js');
 
 test('creates an enemy object', () => {
-    const enemy = new Enemy('goblin', 'sword');
+  const enemy = new Enemy('goblin', 'sword');
 
-    expect(enemy.name).toBe('goblin');
-    expect(enemy.weapon).toBe('sword');
-    expect(enemy.health).toEqual(expect.any(Number));
-    expect(enemy.strength).toEqual(expect.any(Number));
-    expect(enemy.agility).toEqual(expect.any(Number));
-    expect(enemy.potion).toEqual(expect.any(Object));
+  expect(enemy.name).toBe('goblin');
+  expect(enemy.weapon).toBe('sword');
+  expect(enemy.health).toEqual(expect.any(Number));
+  expect(enemy.strength).toEqual(expect.any(Number));
+  expect(enemy.agility).toEqual(expect.any(Number));
+  expect(enemy.potion).toEqual(expect.any(Object));
 });
 
-Enemy.prototype.getHealth = function() {
-  return `The ${this.name}'s health is now ${this.health}!`;
-};
+test("gets enemy's health value", () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-Enemy.prototype.isAlive = function() {
-  if (this.health === 0) {
-    return false;
-  }
-  return true;
-};
+  expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
 
-Enemy.prototype.getAttackValue = function() {
-  const min = this.strength - 5;
-  const max = this.strength + 5;
+test('gets a description of the enemy', () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-  return Math.floor(Math.random() * (max - min) + min);
-};
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+  expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+});
 
-Enemy.prototype.reduceHealth = function(health) {
-  this.health -= health;
+test('checks if enemy is alive or not', () => {
+  const enemy = new Enemy('goblin', 'sword');
 
-  if (this.health < 0) {
-    this.health = 0;
-  }
-};
+  expect(enemy.isAlive()).toBeTruthy();
 
-Enemy.prototype.getHealth = function() {
-  return `The ${this.name}'s health is now ${this.health}!`;
-};
+  enemy.health = 0;
 
-Enemy.prototype.isAlive = function() {
-  if (this.health === 0) {
-    return false;
-  }
-  return true;
-};
+  expect(enemy.isAlive()).toBeFalsy();
+});
 
-Enemy.prototype.getAttackValue = function() {
-  const min = this.strength - 5;
-  const max = this.strength + 5;
+test("gets enemy's attack value", () => {
+  const enemy = new Enemy('goblin', 'sword');
+  enemy.strength = 10;
 
-  return Math.floor(Math.random() * (max - min) + min);
-};
+  expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+  expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
 
-Enemy.prototype.reduceHealth = function(health) {
-  this.health -= health;
+test("subtracts from enemy's health", () => {
+  const enemy = new Enemy('goblin', 'sword');
+  const oldHealth = enemy.health;
 
-  if (this.health < 0) {
-    this.health = 0;
-  }
-};
+  enemy.reduceHealth(5);
 
-Enemy.prototype.getDescription = function () {
-    return `A ${this.name} holding a ${this.weapon} has appeared!`;
-}
+  expect(enemy.health).toBe(oldHealth - 5);
+
+  enemy.reduceHealth(99999);
+
+  expect(enemy.health).toBe(0);
+});
